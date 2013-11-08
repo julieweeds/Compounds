45f4215a9e7cb7eb60fe6f446c25331ea2ac49fe
Julie Weeds <julie.weeds@gmail.com>
2013-11-08 10:41:27 +0000

    tagging errors debugging

--- src/compare/compose.py
+++ src/compare/compose.py
@@ -200,6 +200,11 @@ class Composer:
                         except TaggingError:
                             print "Ignoring "+fields[0]
                             break
+
+                        try:
+                            headmatch=untag(headmatch)[0]  #adjs are tagged, nouns aren't!
+                        except TaggingError:
+                            pass
  #                       isheadmatch=untag(collocmatch.split(':')[0])[0]
                         isheadmatch=collocmatch.split(':')[2]
 
@@ -216,7 +221,7 @@ class Composer:
                         added+=1
 
                     else:
-                       print "No match for "+headmatch, str(self.headmatch.get(headmatch,0))
+                       print "No match for "+headmatch, str(self.headdict.get(headmatch,0))
                     if self.parameters['testing'] and linesread%1000==0:print "Read "+str(linesread)+" lines and copying "+str(added)+" vectors"
             print "Read "+str(linesread)+" lines"
             print "Copying "+str(added)+" vectors"
