0b7cc67584a05e0112278b5720ba9e3394146662
julieweeds <julie.weeds@gmail.com>
2014-05-23 10:50:32 +0100

    debugging extra thesaurus entries

--- src/WNcomp/compare.py
+++ src/WNcomp/compare.py
@@ -123,7 +123,8 @@ class ThesEntry:
         if len(self.neighdict.keys())>0:
             print "Warning: neighbours already added for "+self.phrase
             print self.neighdict
-            print fields[0:10]
+            print fields[-10:len(fields)]
+            exit()
 
         #print fields
         while len(self.neighdict.keys()) < k and len(fields)>0:
